313b377fddc381eb1fcd95ba7d7f5113c1afb0a0
BuildTools <unconfigured@null.spigotmc.org>
2016-06-06 20:07:33 -0400

    Added settings and pom.xml

---
+++ src/com/snowleapord1863/APTurrets/Settings.java
@@ -0,0 +1,16 @@
+package blub;
+
+import java.util.Map;
+import java.util.Set;
+
+public class Settings {
+	 public static Set<String> TurretName;
+	    public static Map<String, String> SignSecondLine;
+		public static Map<String, Double> SignCost;
+		public static Map<String, String> SignPermission;
+		public static Map<String, String> UsePermission;
+		public static Map<String, String> Entity;
+		public static Map<String, Integer> Velocity;
+	    public static Map<String, Boolean> OnFire;
+	    public static Map<String, Boolean> RequiresAmmo;
+}
